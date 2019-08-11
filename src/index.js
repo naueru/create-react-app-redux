4f4893b5b65d96e407535b2c7b818c0f6ff9d7d5
Nahuel Andrada <adolfonahuel@gmail.com>
2019-08-11 00:05:53 -0300

    Adds imports comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+// Core
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+
+// Store
 import store, { history } from './store';
+
+// Components
 import App from './containers/app';
 
+
+// Styles
 import 'sanitize.css/sanitize.css';
 import './index.css';
 
